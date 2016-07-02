23bb2070dacee204c8aafa756e138cafe8e028f0
Piotr Kuszaj <peterkuszaj@gmail.com>
2016-07-02 12:09:38 +0200

    Bitwise operators SFINAE structs

---
+++ include/SFEC/member/_gen_bitwise_operators.h
@@ -0,0 +1,19 @@
+#ifndef SFEC_MEMBER__GEN_BITWISE_OPERATORS_H_
+#define SFEC_MEMBER__GEN_BITWISE_OPERATORS_H_
+
+#include <SFEC/member/decl_has_function_member.h>
+
+namespace SFEC {
+namespace member {
+
+SFEC_declHasOperatorType(operator~,  _gen_bitwise_not);    // General bitwise NOT (should not be used)
+SFEC_declHasOperatorType(operator&,  _gen_bitwise_and);    // General bitwise AND (should not be used)
+SFEC_declHasOperatorType(operator|,  _gen_bitwise_or);     // General bitwise OR (should not be used)
+SFEC_declHasOperatorType(operator^,  _gen_bitwise_xor);    // General bitwise XOR (should not be used)
+SFEC_declHasOperatorType(operator<<, _gen_bitwise_lshift); // General bitwise left shift (should not be used)
+SFEC_declHasOperatorType(operator>>, _gen_bitwise_rshift); // General bitwise right shift (should not be used)
+
+} /* SFEC::member */
+} /* SFEC */
+
+#endif /* SFEC_MEMBER__GEN_BITWISE_OPERATORS_H_ */
---
+++ include/SFEC/member/bitwise_operators.h
@@ -0,0 +1,36 @@
+#ifndef SFEC_MEMBER_BITWISE_OPERATORS_H_
+#define SFEC_MEMBER_BITWISE_OPERATORS_H_
+
+#include <SFEC/member/_gen_bitwise_operators.h>
+
+namespace SFEC {
+namespace member {
+
+template <typename T, typename Ret>
+using has_operator_bitwise_not    // Bitwise NOT
+    = has_operator__gen_bitwise_not<T, Ret>;
+
+template <typename T, typename Ret, typename Arg>
+using has_operator_bitwise_and    // Bitwise AND
+    = has_operator__gen_bitwise_and<T, Ret, Arg>;
+
+template <typename T, typename Ret, typename Arg>
+using has_operator_bitwise_or     // Bitwise OR
+    = has_operator__gen_bitwise_or<T, Ret, Arg>;
+
+template <typename T, typename Ret, typename Arg>
+using has_operator_bitwise_xor    // Bitwise XOR
+    = has_operator__gen_bitwise_xor<T, Ret, Arg>;
+
+template <typename T, typename Ret, typename Arg>
+using has_operator_bitwise_lshift // Bitwise left shift
+    = has_operator__gen_bitwise_lshift<T, Ret, Arg>;
+
+template <typename T, typename Ret, typename Arg>
+using has_operator_bitwise_rshift // Bitwise right shift
+    = has_operator__gen_bitwise_rshift<T, Ret, Arg>;
+
+} /* SFEC::member */
+} /* SFEC */
+
+#endif /* SFEC_BITWISE_OPERATORS_H_ */
