f838b9d887cf256a1f5ae836df4ef8365ce06377
Piotr Kuszaj <peterkuszaj@gmail.com>
2016-07-02 12:12:08 +0200

    Collective header file for member operators

---
+++ include/SFEC/member/operators.h
@@ -0,0 +1,12 @@
+#ifndef SFEC_MEMBER_OPERATORS_H_
+#define SFEC_MEMBER_OPERATORS_H_
+
+#include <SFEC/member/constructor.h>
+
+#include <SFEC/member/arithmetic_operators.h>
+#include <SFEC/member/bitwise_operators.h>
+#include <SFEC/member/compound_assignment_operators.h>
+#include <SFEC/member/logical_operators.h>
+#include <SFEC/member/relational_operators.h>
+
+#endif /* SFEC_MEMBER_OPERATORS_H_ */
