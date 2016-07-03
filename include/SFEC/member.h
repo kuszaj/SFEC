2f27314ab4d32422f6a9d7473061514b214d51c1
Piotr Kuszaj <peterkuszaj+github@gmail.com>
2016-07-03 19:06:01 +0200

    Adding new include

--- include/SFEC/member.h
+++ include/SFEC/member.h
@@ -1,7 +1,9 @@
 #ifndef SFEC_MEMBER_H_
 #define SFEC_MEMBER_H_
 
-#include <SFEC/member/operators.h>
+#include <SFEC/member/decl_has_data_member.h>
 #include <SFEC/member/decl_has_function_member.h>
+
+#include <SFEC/member/operators.h>
 
 #endif /* SFEC_MEMBER_H_ */
